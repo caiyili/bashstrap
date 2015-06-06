f60f6f586e851dd66e4171baca738fc4764be4ef
CaiYili <caiyili@baijiahulian.com>
2015-06-06 15:44:35 +0800

    更改安装脚本

--- .bash_profile
+++ .bash_profile
@@ -112,3 +112,4 @@ export PROMPT_COMMAND='echo -ne "\033]0;${PWD##*/}\007"'
 
 # init z! (https://github.com/rupa/z)
 #. ~/z.sh
+source ~/.bashrc.d/*.sh
--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh 
 
-BACKUP_DIR="~/backup"
+TIME=`date +"%Y%m%d%H%M%S"`
+BACKUP_DIR="$HOME/backup/$TIME"
 SCRIPT_DIR=`dirname $0`
 
 [[ ! -d $BACKUP_DIR ]] && mkdir -p $BACKUP_DIR
-mv ~/.bashrc ~/.bash_profile ~/.bashrc.d $BACKUP
+mv -f ~/.bashrc ~/.bash_profile ~/.bashrc.d $BACKUP_DIR
 cd $SCRIPT_DIR
 cp -r .bashrc .bash_profile .bashrc.d $HOME
 
